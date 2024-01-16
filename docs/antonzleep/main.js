f265abf72a1b276e1ff2fdaa3d17fc4f04a30032
MattisLindmark <64585570+MattisLindmark@users.noreply.github.com>
2024-01-16 22:42:43 +0100

    Fixat alarmfunktion
    Men det är lite enformigt spel.
    Sätt alarmet så långt åt höger som möjligt.
    och sen försöka komma dit. blir som inte så spännande... Man kanske ska köra fram och tillbaks funktion.

--- docs/antonzleep/main.js
+++ docs/antonzleep/main.js
@@ -17,15 +17,15 @@ l    l
    l
   lllll
 `,
-`
+  `
 yyyy
 y 
  y
   y
 yyyy
 `
-,
-`
+  ,
+  `
 ll  ll
  llll
 lll ll
@@ -46,9 +46,10 @@ options = {
   viewSize: { x: G.WIDTH, y: G.HEIGHT },
   isReplayEnabled: false,
   isCapturing: true,
-  seed: 2,
-  theme: "dark"
-
+  seed: 55,
+  theme: "dark",
+  
+// s2 = använde nyss (hit och explotion). seed 44 ganska bra select och laser. s53 random. s55 coin/jump alarm. click sleep. Random gameover.
 };
 
 /**
@@ -61,18 +62,24 @@ let alarmClock;
 let alarmPosition = 50;
 let sleepTime = 0;
 let day = 1;
+let baseAx = 0.01;
+let ax = 0.01;
+let isInTheGreen = false;
+
 /**
  * @typedef { object } GameState - What is the state of current dayloop
  * @property { number } state - 1- set alarm, 2- sleep, 3- wake up
  */
 
 let gameState = 1;
+let transitionFunc = switchStateScreen;
 
 function update() {
   if (!ticks) {
     setupCharacters();
+    //playBgm();
   }
-  
+
   if (gameState == 1) {
     handleAlarmstate();
   } else if (gameState == 2) {
@@ -80,22 +87,27 @@ function update() {
     handleSleepstate();
   } else if (gameState == 3) {
     handleWakeupstate();
-  } else {
-    switchStateScreen();
+  } else if (gameState == 0) {
+    transitionFunc();
   }
 
 
 }
 
 function handleSleepstate() {
+  
+  color("blue");
+  char("a", sleepTime+1, 45);
+
   color("light_yellow");
   char(alarmClock.sprite, alarmClock.pos);
-
-  color("blue");
-  char("a", 50, 42);
+  color("light_green");
+  box(alarmPosition, 50, 7, 7);
+  color("red");
+  box(alarmPosition+5, 50, 4, 7);
 
   if (input.isPressed && sleepTime < 101) {
-    if (sleepTime < 40) {
+    if (sleepTime < 30) {
       color("light_black");
     } else if (sleepTime < 70) {
       color("green");
@@ -104,9 +116,22 @@ function handleSleepstate() {
     }
 
     text(getSovaText(), 1, 60);
-    sleepTime += 1;
-    box(50, 50, sleepTime, 10);
-    play("hit");
+    sleepTime += .2 + ax;
+    ax += baseAx;
+    //    box(50, 50, sleepTime, 10);
+    let stapel = bar(2, 50, sleepTime, 5, 0, 0);
+
+    if (stapel.isColliding.rect.light_green) {
+         isInTheGreen = true;
+    }
+    if (stapel.isColliding.rect.red) {
+      play("powerUp");
+      isInTheGreen = false;
+      sleepTime = 101;
+    }
+
+
+    play("explosion", { volume: 0.5 });
   }
 
   if (sleepTime >= 101) {
@@ -115,12 +140,13 @@ function handleSleepstate() {
 
 
     color("red");
-    box(50, 50, 100, 10);
+    bar(2, 50, 100, 10, 0, 0);
+    //    box(50, 50, 100, 10);
     sleepTime += 1;
   }
 
   if (sleepTime == 110) {
-    play("explosion");
+    play("random");
   }
 
   if (sleepTime > 170) {
@@ -130,37 +156,52 @@ function handleSleepstate() {
     end("");
   }
 
-  if (input.isJustReleased && sleepTime < 40) {
+  if (input.isJustReleased && sleepTime < 30) {
     sleepTime = 0;
+    ax = baseAx;
   }
 
-  if (input.isJustReleased && sleepTime > 40 && sleepTime < 101) {
-    score += sleepTime;
-    switchState(3, "Heeejjjaaa hej!!");
+  if (input.isJustReleased && sleepTime > 30 && sleepTime < 101 && isInTheGreen == true) {
+    score += (sleepTime*10);
+    isInTheGreen = false;
+    transition(3, "RIIiiiiiing riiing...");
     //gameState = 3;
     // reset();
     // play("powerUp");
     // text("sovtid: " + score, 3, 70);
     // end("Du vaknade!");
+  } else if (input.isJustReleased && sleepTime > 30 && sleepTime < 101 && isInTheGreen == false) {
+    score -= (sleepTime*5);
+    transition(2, "snooze");
+    //gameState = 2;
   }
+
+
 }
 
-function switchState(newState, text)
-{
-  gameState = 0;  
+
+function transition(newState, text) {
+  transitionFunc = switchStateScreen.bind(null, text);
+  gameState = 0;
   // Paus execution for 1 second
-  setTimeout(() => {gameState = newState;}, 1000);
-  
- // play("powerUp");
+  setTimeout(() => { gameState = newState; }, 1000);
+
+  // play("powerUp");
 }
 
-function switchStateScreen()
-{
-  color("light_black");
-  text("Wait for it!", 15, 40);
-  
+function quickTransition(newState) {
+  gameState = -1;
+  setTimeout(() => { gameState = newState; }, 100);
 }
-function setupCharacters(){
+
+function switchStateScreen(txt = "waitforit") {
+ // console.log("switchStateScreen");
+  play("coin");
+  color("light_black");
+  text(txt, 15, 40);
+}
+
+function setupCharacters() {
   zsymbol = times(20, () => {
     const posX = rnd(0, G.WIDTH);
     const posY = rnd(G.HEIGHT, G.HEIGHT);
@@ -168,17 +209,17 @@ function setupCharacters(){
       // Creates a Vector
       pos: vec(posX, posY),
       speed: rnd(G.Z_SPEED, G.Z_SPEED_MAX),
-      sprite: rnd(1)<0.6? "b" : "c"
+      sprite: rnd(1) < 0.6 ? "b" : "c"
     };
   });
 
   alarmClock = {
-    pos: vec(G.WIDTH-5, 42),
+    pos: vec(G.WIDTH - 50, 42),
     speed: 1,
     sprite: "d"
   };
 }
-function writeZleepFX(){
+function writeZleepFX() {
   // if (!ticks) {
   //   // A CrispGameLib function
   //   // First argument (number): number of times to run the second argument
@@ -201,16 +242,16 @@ function writeZleepFX(){
     s.pos.y -= s.speed;
     s.pos.wrap(0, G.WIDTH, 0, G.HEIGHT); // warps to top of screen when it reaches bottom
     color("light_green");
-    if (sleepTime > 100){
+    if (sleepTime > 100) {
       color("light_red");
     };
     char(s.sprite, s.pos);
   });
 }
 
-function getSovaText(){
+function getSovaText() {
   if (sleepTime < 50) return "sover sover sover";
-  if (sleepTime < 80 ) return "sover liiite till";
+  if (sleepTime < 80) return "sover liiite till";
   if (sleepTime < 90) return "...";
   return "";
 }
@@ -220,31 +261,33 @@ function reset() {
   sleepTime = 0;
   G.Z_SPEED_MAX = 1.0;
   gameState = 1;
+  ax = baseAx;
   // Reset other variables as needed...
 }
 
 function handleWakeupstate() {
-       reset();
-       play("powerUp");
-       text("sovtid: " + score, 3, 70);
-       end("Du vaknade!");
+  reset();
+//  play("powerUp");
+  text("sovtid: " + score, 3, 70);
+  end("Du vaknade!");
 }
 function handleAlarmstate() {
   color("light_yellow");
-  text("Press set alarm", 5, rnd(25,28));
+  text("set alarm", 30, rnd(26, 28));
 
   // char c going back and forth over the screen
   color("yellow");
   alarmClock.pos.x -= alarmClock.speed;
-  if (alarmClock.pos.x < 10 || alarmClock.pos.x > G.WIDTH-5) {
+  if (alarmClock.pos.x < 35 || alarmClock.pos.x > G.WIDTH - 2) {
     alarmClock.speed *= -1;
   }
   char(alarmClock.sprite, alarmClock.pos);
 
   if (input.isJustPressed) {
     alarmPosition = alarmClock.pos.x;
-    gameState = 2;
-    play("lucky");
+    play("hit", { volume: 0.5 });
+//    gameState = 2;
+    quickTransition(2);
   }
 }
 
