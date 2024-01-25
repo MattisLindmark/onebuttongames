9ec76e0aa080f4ad378f5e5c4c3db54270d68d07
MattisLindmark <64585570+MattisLindmark@users.noreply.github.com>
2024-01-25 22:27:36 +0100

    Gjort webbsida
    Kanske går att komma åt på www/index.html på vanliga adressen. lixom.

---
+++ docs/www/autogenerate.js
@@ -0,0 +1,38 @@
+// read titles.json and generate html based on the class styles.css
+// and the template template.html
+// using javascript
+
+//var fs = require('fs');
+//var path = require('path');
+var path = "titles.json";
+var gamePath = "https://mattislindmark.github.io/onebuttongames/?";
+
+
+// read the file and parse the json. Using fetch() instead of readFileSync()
+let titles = [];
+fetch(path)
+  .then(response => response.json())
+  .then(data => {
+    // Now data is the parsed JSON object from the URL
+    titles = data.games;
+    console.log(titles);
+    printTheHTML(titles);    
+    console.log("hoj"+titles[0].title);
+  })
+  .catch(error => console.error('Error:', error));
+
+  function printTheHTML(titles) {
+    var html = "";
+    titles.forEach(title => {
+        html += `
+        <div class="item">
+            <h2>${title.title}</h2>
+            <img src="../${title.folderName}/screenshot.gif" alt="${title.title} Screenshot">
+            <p>${title.description}</p>
+            <div class="play-button">
+            <a href="${gamePath}${title.folderName}">Play</a>
+            </div>
+        </div>`;
+    });
+    document.getElementById("games").innerHTML = html;
+}
