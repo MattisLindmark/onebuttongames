b554be4e83521eafc0812307cc8d6ecfdc41df5d
MattisLindmark <64585570+MattisLindmark@users.noreply.github.com>
2024-02-14 23:32:16 +0100

    Fone
    Nu med sound från en oscillator. MEN: Den är problematisk när man tex laddar om sidan så kan den inte starta igen.
    Möjligen att jag behöver skapa den första ticken. Men det borde inte åtgärda problemet... hmm.

--- docs/fone/main.js
+++ docs/fone/main.js
@@ -75,25 +75,73 @@ function car( tire, fule) {
   this.fule = fule;
 }
 
+/* Ljudtest */
+// Create an AudioContext
+let audioContext = new (window.AudioContext || window.webkitAudioContext)();
+let motorsound = audioContext.createOscillator();
+motorsound.type = 'sawtooth';
+motorsound.frequency.value = 60;
+let detuneValue = 1000;
+
+motorsound.detune.value = detuneValue;
+
+// Create a GainNode
+let gainNode = audioContext.createGain();
+
+// Connect the oscillator to the GainNode
+motorsound.connect(gainNode);
+
+// Connect the GainNode to the destination
+gainNode.connect(audioContext.destination);
+// Set the volume
+gainNode.gain.value = 0; // 50% volume
+motorsound.start();
+
+// motorsound.start();
+// setTimeout(() => {
+//     motorsound.stop();
+// }, 5000);
+let detuneMod = 0;
+
 let playerCar;
 // ======================================================Update========================
 function update() {
   if (!ticks) {
+    detuneMod = 0;
     fullReset();
     playerCar = new car(100, 100);
-    sss.setVolume(0.1);
+    sss.setVolume(0.3);
+    console.log("THIS is the first");
   }
+
+  
   playerCar.fule = clamp(playerCar.fule, 0, 100);
   playerCar.tire = clamp(playerCar.tire, 0, 100);
-
+  
+  // green background
+  color("light_green");
+  rect(0, 0, G.WIDTH, G.HEIGHT);
+  color("black");
+  
+  
   if (firstRound){
     drawTrack2();
     return;
   }
-
-  //char("b", 98, 103, {scale: {x: 1.2, y: 1}});
   
+  //char("b", 98, 103, {scale: {x: 1.2, y: 1}});
+ 
   drawTrack2();
+
+  if (nrOfLaps >= maxNrOfLaps - 1) {
+    color("white");
+    char("e", 155, 100, { scale: { x: 2, y: 2 } });
+    color("black");
+  } else {
+    color("black");
+    char("e", 150, 100, { scale: { x: 1, y: 1 } });
+  }
+
   text("Laps: " + nrOfLaps+" / "+maxNrOfLaps, 50, 3);
  
   if (char("b", 180, 100, {scale: {x: 2, y: 2}}).isColliding.char.a) {  
@@ -109,9 +157,11 @@ function update() {
   rect(80, 70, playerCar.tire/2, 4);
   rect(80, 76, playerCar.fule/2, 4);
 
+// HÄÄÄÄR
 
   if (playerCar.tire < 1 || playerCar.fule < 1)
   {
+    motorsoundOnOff(false);
     play("explosion", {volume: 0.3});
     end();
 //    fullReset();
@@ -119,8 +169,20 @@ function update() {
 
   if (nrOfLaps >= maxNrOfLaps)
   {
+    motorsoundOnOff(false);
     play("powerUp", {volume: 0.3});
     complete();
+  }
+}
+
+function motorsoundOnOff(onOff) {
+  if (onOff)
+  {
+//    motorsound.start();
+    gainNode.gain.value = 0.05;
+  } else {
+//    motorsound.stop();
+    gainNode.gain.value = 0;
   }
 }
 
@@ -140,17 +202,25 @@ let firstRound = true;
 function drawTrack2() {
   
   if (input.isPressed) {
+    detuneMod += 2;
     speedModifier += 0.1;
     throttle += 0.008;
   }else{
+    detuneMod -= 1;
     speedModifier -= 0.01;
     throttle -= 0.01;
+    motorsound.detune.value = detuneValue;
   }
+
+  detuneMod = clamp(detuneMod, 0, 300);
+  motorsound.detune.value = detuneValue + detuneMod;
+    
+
   speedModifier = clamp(speedModifier,1.5,2.5);
   
     if (firstRound)
     {
-      nrOfLaps = -1;
+      nrOfLaps = -2;
       speedModifier = 0;
       angle = 12.2;
       throttle = 2.15;
@@ -158,13 +228,15 @@ function drawTrack2() {
       {
         firstRound = false;
         speedModifier = 0.01;
+//        motorsound.start();
+        motorsoundOnOff(true);
         //throttle = 2;
        // angle = 0;
       }
     }
   
 
-  throttle = clamp(throttle,0,3);
+  throttle = clamp(throttle, 0, 3);
   color("light_black");
   arc(100, 85, 60, 12, PI, 2 * PI);
   arc(100, 120, 60, 12, 0, PI);
@@ -172,9 +244,9 @@ function drawTrack2() {
   line(40, 85, 40, 120, 12);
   line(160, 85, 160, 120, 12);
 
-color("light_black");
-arc(45, 102, 20, 4, PI / 1.5, 3 * PI / 2.2);
-arc(155, 102, 20, 4, PI / 1.5 + PI, 3 * PI / 2.2 + PI);
+  color("light_black");
+  arc(45, 102, 20, 4, PI / 1.5, 3 * PI / 2.2);
+  arc(155, 102, 20, 4, PI / 1.5 + PI, 3 * PI / 2.2 + PI);
 
 
   drawThinTrack(100, 85, 60,1); 
@@ -215,8 +287,16 @@ if (firstRound)
 }
 
 let coltimer = 0;
+//let comboBonus = 0;
 let offTrack = false;
 function checkCollisions(colData){
+  
+  // // a bar that shows comboBonus
+  // color("black");
+  // rect(20, 20, 100, 5);
+  // color("yellow");
+  // rect(20, 20, comboBonus * 10, 2);
+  // color("black");
 
   if (colData.isColliding.rect.light_black || colData.isColliding.rect.green)
   {
@@ -238,16 +318,20 @@ function checkCollisions(colData){
       if (coltimer % 40 == 0)
       {
         play("coin", {volume: 0.1});
-        score += 1;
+//        comboBonus+=0.2;
+//        comboBonus = clamp(comboBonus,0,1);
+        score += 1;// + Math.floor(comboBonus);
       }
 
     } else {
       innerTrackColor = "green";
+     // comboBonus = 0;
       coltimer = 0;
     }
   } else {
     if (offTrack == false) {
       coltimer = 19;
+     // comboBonus = -1;
       offTrack = true;
     }
     coltimer ++;
