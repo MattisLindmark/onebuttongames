178200c87d5c14cc084ff6ba4adb409e6b1894e9
MattisLindmark <64585570+MattisLindmark@users.noreply.github.com>
2024-05-01 22:09:02 +0200

    Simple bowling är klart
    tror jag.

--- docs/bowling/main.js
+++ docs/bowling/main.js
@@ -1,6 +1,6 @@
-title = "";
+title = "simple\n BOWLING";
 
-description = `
+description = `strike 10p\n spare 5p\n 5 rounds
 `;
 
 characters = [
@@ -35,10 +35,11 @@ ll
 
 const G = {
   WIDTH: 110,
-  HEIGHT: 190,  
+  HEIGHT: 190,
+  NrOfRounds: 5
 };
 
-const ballStartY = G.HEIGHT - 50;
+const ballStartY = G.HEIGHT - 40;
 
 let GutterLeft = 30;
 let GutterRight = G.WIDTH - 30;
@@ -46,10 +47,10 @@ let GutterRight = G.WIDTH - 30;
 options = {
   viewSize: { x: G.WIDTH, y: G.HEIGHT },
   //isPlayingBgm: true,
-  isReplayEnabled: true,
+  //isReplayEnabled: true,
   //  seed: 1,
   //  isShowingScore: false,
-  //  theme: "crt",
+    theme: "shapeDark",
   //  isShowingTime: true,
   //  isCapturing: true,
   //  captureCanvasScale: .2,
@@ -172,13 +173,26 @@ class bowlingBall {
           this.vel.x *= -1;
         }else{
           this.vel.x = 0;
-        }    
+        }
+        if (this.pos.y > 5)
+        {
+          Gutterflag = true;
+          //play("explosion");
+        }
       }
 
       if (this.pos.y < -100) {
         console.log("ball out of bounds");
         this.isLaunched = false;
         FinishTurn = true;
+
+        if (this.pos.x < GutterLeft || this.pos.x > GutterRight) {
+          if (Gutterflag){
+            gameStats.totalGutterBalls++;
+            Gutterflag = false;
+          }
+        }
+
       }
       // if (this.pos.x < 0 || this.pos.x > G.WIDTH) {
       //   this.vel.x *= -1;
@@ -206,28 +220,83 @@ let FinishTurn = false;
 let runda = 1;
 let turn = 1;
 
+let vTimer = {
+  strikeText: 0,
+  spareText: 0,
+  roundText: 0,
+  pinsleftText: 0
+};
+
+let gameStats = {
+  totalPins: 0,
+  totalStrikes: 0,
+  totalSpares: 0,
+  totalGutterBalls: 0,    
+}
+let Gutterflag = false;
+let GOtimer = 0;
 let CHEATMODE = false;
 
  //================================================================= MAIN LOOP MARK: update
 function update() {
   if (!ticks) {
+    sss.setSeed(8);
+    sss.setVolume(0.05);
+    Gutterflag = false;
+    GOtimer = 0;
+    resetGameStats();
+    resetStuff();
     //    sss.setSeed(5);
     //    sss.setVolume(0.05);
     ball = new bowlingBall();
     setupPind();
   }
+  if (ticks < 20) {
+    return;
+  }
 
-  text("y"+ball.pos.y, 3, 13);
+  // if (input.isJustReleased) {
+  //   play("select");
+  // }
+  if (runda > G.NrOfRounds) {
+    GOtimer++;
+    let y = G.HEIGHT*0.2;
+    text("GOOD JOB!", 6,y-5,{scale:{x:2,y:2}});
+    text("Score:   " + score, G.WIDTH * 0.2, y+10);
+    text("Pins:    " + gameStats.totalPins, G.WIDTH * 0.2, y+20);
+    text("Strikes: " + gameStats.totalStrikes, G.WIDTH * 0.2, y+30);
+    text("Spares:  " + gameStats.totalSpares, G.WIDTH * 0.2, y+40);
+    if (gameStats.totalGutterBalls > 0)
+    {
+    color("light_black")
+    text("(" + gameStats.totalGutterBalls+" in the gutter)", G.WIDTH * 0.1, G.HEIGHT-20);
+    }
+    color("black");
+    if (input.isJustPressed || GOtimer > 300) {
+      text("  press\n any key", G.WIDTH * 0.5-20, G.HEIGHT * 0.5);
+      end("");
+    }
+    return;
+  }
+    
+
+  //text("y"+ball.pos.y, 3, 13);
+  text(runda+"/5", 3, 15);
+
 
   if (FinishTurn) {
     handleEndOfTurn();
     ball = new bowlingBall();
     FinishTurn = false;
   }
+color ("light_black");
+  text("^",G.WIDTH / 2, ballStartY - 35);
+  text("^ ^",G.WIDTH / 2-6, ballStartY - 30);
+  text("^   ^",G.WIDTH / 2-12, ballStartY - 25);
+  color("black");
+  
 
-  text("^",G.WIDTH / 2, G.HEIGHT/2-15);
-
-  color("red");
+  color("light_red");
   rect(GutterLeft-10, 0, 10, G.HEIGHT);
   rect(GutterRight, 0,10, G.HEIGHT);
 
@@ -247,10 +316,50 @@ function update() {
     ball.speed = 1;
     ball.launch();
   }
+
+
+  // Show Stuff
+  // if (ticks <2)
+  // {
+  //   vTimer.pinsleftText = 100;
+  //   pinsleft = 5;
+  //   vTimer.strikeText = 100;
+  //   vTimer.spareText = 100;
+  // }
+
+
+  if (vTimer.roundText > 0) {
+    vTimer.roundText--;
+    let t = Math.floor(vTimer.roundText / 5) % 4;
+    color("green");
+    text("Round: " + runda, (G.WIDTH * 0.5)-20, G.HEIGHT*0.3);
+    color("black");
+  }
+
+  if (vTimer.spareText > 0) {
+    vTimer.spareText--;
+    let t = Math.floor(vTimer.spareText / 5) % 4;
+    color(colors[t]);
+    text("SPARE", (G.WIDTH * 0.5)-25, G.HEIGHT*0.15,{scale: {x: 2, y: 2}});
+    color("black");
+  }
+
+
+  if (vTimer.strikeText > 0) {
+    vTimer.strikeText--;
+    let t = Math.floor(vTimer.strikeText / 5) % 4;
+    let y = lerp(10,80,vTimer.strikeText * 0.01);
+    color(colors[t]);
+    text("STRIKE", (G.WIDTH * 0.5)-30, y,{scale: {x: 2, y: 2}});
+  }
+  if (vTimer.pinsleftText > 0) {
+    vTimer.pinsleftText--;
+//    let y = lerp(10,80,Math.sin(ticks * 0.05));
+    color("black");
+    text("Pins\nleft:" + pinsleft, (G.WIDTH * 0.5)-20, G.HEIGHT*0.3);
+  }
+
 }
-
-
-
 
 
 let swayAngle = 0;
@@ -308,7 +417,7 @@ function drawAim() {
   }
 }
 
-
+let pinsleft = 10;
 function handleEndOfTurn() {
   // if (runda == 1) {
   //   runda = 2;
@@ -318,8 +427,9 @@ function handleEndOfTurn() {
   // }
 
   pins.forEach((pin) => {
-    if (pin.hasFallen) {
+    if (pin.hasFallen && pin.inPlay) {
       score++;
+      gameStats.totalPins++;
       pin.inPlay = false;
     }
   });
@@ -329,17 +439,26 @@ function handleEndOfTurn() {
   if (pinsInPlay.length == 0) {
     if (turn == 1) {
       score += 10;
-      console.log("Strike");
+//      console.log("Strike");
+      vTimer.strikeText = 60;
+      play("powerUp");
+      gameStats.totalStrikes++;
       turn = 2;
     } else {
       score += 5;
+      vTimer.spareText = 60;
+      play("select");
+      gameStats.totalSpares++;
       console.log("Spare");
     }
-  } else {
+  } else if (turn == 1) {
     console.log("Pins left: " + pinsInPlay.length);
+    vTimer.pinsleftText = 60;
+    pinsleft = pinsInPlay.length;
   }
 
   if (turn > 1) {
+    vTimer.roundText = 100;
     console.log("End of turn");
     console.log("Score: " + score);
     console.log("Turn: " + turn);
@@ -378,12 +497,41 @@ function setupPind() {
   }
 }
 
+function resetGameStats() {
+  gameStats.totalPins = 0;
+  gameStats.totalStrikes = 0;
+  gameStats.totalSpares = 0;
+  gameStats.totalGutterBalls = 0;
+}
+
+function resetStuff() {
+  score = 0;
+  runda = 1;
+  turn = 1;
+  FinishTurn = false;
+  vTimer = {
+    strikeText: 0,
+    spareText: 0,
+    roundText: 0,
+    pinsleftText: 0
+  };
+}
 
 //========= MARK: - helpers
 function calculateAngle(start, end) {
   return Math.atan2(end.y - start.y, end.x - start.x);
 }
 
+const colors = ["red", "blue", "green", "yellow", "purple", "cyan", "black"];
+const allColors = ["red", "blue", "green", "yellow", "purple", "cyan", "black", "light_red", "light_blue", "light_green", "light_yellow", "light_purple", "light_cyan", "light_black"];
+
+function lerp(start, end, t) {
+  return start * (1 - t) + end * t;
+}
+
+function easInOut (t) {
+  return t<.5 ? 2*t*t : -1+(4-2*t)*t;
+} 
 
 /*
       pins.forEach((pin) => {
